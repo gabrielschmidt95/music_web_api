13331c75059c3b60585393674d3de13f2f8b0ce1
Gabriel Schmidt <gabrieloliveira95@live.com>
2022-07-28 00:52:09 +0100

    Test Change

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,17 +1,32 @@
 from data.data_center import MongoDBConn
 from dotenv import load_dotenv
 from typing import NoReturn
-import sys 
+import pandera as pa
+import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 load_dotenv()
 
-COLUMNS = ("_id",'RELEASE_YEAR', 'ARTIST', 'TITLE', 'MEDIA', 'PURCHASE', 'ORIGIN',
-       'EDITION_YEAR', 'IFPI_MASTERING', 'IFPI_MOULD', 'BARCODE',
-       'MATRIZ', 'LOTE','ANO_AQUISICAO','RECENTE','LISTA')
+validate_schema = pa.DataFrameSchema({
+    "RELEASE_YEAR": pa.Column(int),
+    "ARTIST": pa.Column(str),
+    "TITLE": pa.Column(str),
+    "MEDIA": pa.Column(str),
+    "PURCHASE": pa.Column(object, nullable=True),
+    "ORIGIN": pa.Column(str, nullable=True),
+    "EDITION_YEAR": pa.Column(float, nullable=True),
+    "IFPI_MASTERING": pa.Column(str, nullable=True),
+    "IFPI_MOULD": pa.Column(str, nullable=True),
+    "MATRIZ": pa.Column(str, nullable=True),
+    "LOTE": pa.Column(str, nullable=True)
+})
+
 
 def test_data() -> NoReturn:
     df = MongoDBConn(os.environ['CONNECTION_STRING'],
-                 os.environ['DATABASE']).qyery("CD")
-    assert len(df.columns) == len(COLUMNS)
+                     os.environ['DATABASE']).qyery("CD")
+    try:
+       validate_schema(df)
+    except:
+       raise "SCHEMA ERROR"   

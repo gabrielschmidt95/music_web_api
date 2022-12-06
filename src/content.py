3c0e26116311982e9b740760897240154395175f
Gabriel Schmidt <gabrieloliveira95@live.com>
2022-12-05 22:42:04 -0300

    Change buttons

--- src/content.py
+++ src/content.py
@@ -1,13 +1,17 @@
 from dash import html, dcc, Input, Output, State, callback_context, ALL
 import dash_bootstrap_components as dbc
 import plotly.express as px
-from .config import Config
 from src.track import Track
 from server import app
 from json import loads
 from os import environ
-import pandas as pd
 import requests
+from io import StringIO, BytesIO
+from server import app
+import pandera as pa
+import pandas as pd
+import numpy as np
+import base64
 
 
 class Content:
@@ -15,7 +19,6 @@ class Content:
     def __init__(self, conn):
         self.conn = conn
         self.MAX_INDEX = 3
-        self.config = Config(self.conn)
         self.track = Track(self.conn)
         self.discogs_url = "https://api.discogs.com/database/search"
 
@@ -46,22 +49,27 @@ class Content:
                         result = resp.json()["results"]
                         if len(result) == 0:
                             params.pop("country")
-                            resp = requests.get(self.discogs_url, params=params)
+                            resp = requests.get(
+                                self.discogs_url, params=params)
                             result = resp.json()["results"]
                             if len(result) == 0:
                                 params.pop("barcode")
-                                resp = requests.get(self.discogs_url, params=params)
+                                resp = requests.get(
+                                    self.discogs_url, params=params)
                                 result = resp.json()["results"]
 
                 if len(result) > 0:
                     row["DISCOGS"] = result[0]
-                    row["DISCOGS"]["urls"] = [{"id": r['id'],"uri": r['uri']} for r in result]
+                    row["DISCOGS"]["urls"] = [
+                        {"id": r['id'], "uri": r['uri']} for r in result]
                     row["DISCOGS"]["len"] = len(result)
                     _id = row["DISCOGS"]['id']
                     _type = row["DISCOGS"]['type']
-                    tracks = requests.get(f"https://api.discogs.com/{_type}s/{_id}")
+                    tracks = requests.get(
+                        f"https://api.discogs.com/{_type}s/{_id}")
                     if tracks.status_code == 200:
-                        row["DISCOGS"].update(tracks=tracks.json()["tracklist"])
+                        row["DISCOGS"].update(
+                            tracks=tracks.json()["tracklist"])
 
                     self.conn.replace_one("CD", row["_id"], row)
                 else:
@@ -69,7 +77,7 @@ class Content:
             else:
                 return html.Div(f"Error:{resp.status_code}")
 
-        if "tracks" in row["DISCOGS"]: 
+        if "tracks" in row["DISCOGS"]:
             tracklist = row["DISCOGS"]["tracks"]
         else:
             tracklist = []
@@ -122,6 +130,10 @@ class Content:
 
     def layout(self):
         return html.Div([
+            dcc.Download(id="download_xlsx"),
+            html.Div(id="download_alert"),
+            html.Div(id="upload_alert"),
+            html.Div(id="sync_alert"),
             dbc.Tabs(
                 [
                     dbc.Tab([
@@ -155,15 +167,51 @@ class Content:
                         ], width=12
                         ), label="Ano de Aquisição"),
                     #dbc.Tab(self.track.layout(), label="Rastreio"),
-                    dbc.Tab(self.config.layout(), label="Configuração"),
                 ]
-            )
+            ),
+            dbc.Row([
+                    dbc.Col(
+                        dbc.ButtonGroup(
+                            [
+                                dbc.Button(
+                                    " Download XLSX",
+                                    color="primary",
+                                    className="bi bi-download",
+                                    outline=True,
+                                    id="download_xlsx_btn"
+                                ),
+                                dcc.Upload(
+                                    dbc.Button(
+                                        " Upload XLSX",
+                                        color="primary",
+                                        className="bi bi-upload",
+                                        outline=True,
+                                    ),
+                                    id="upload_xlsx"
+                                ),
+                                dbc.Button(
+                                    " Adicionar",
+                                    color="primary",
+                                    className="bi bi-plus-circle",
+                                    outline=True,
+                                    id="insert_btn"
+                                ),
+                                dbc.Button(
+                                    " Sincronizar Bases",
+                                    color="primary",
+                                    className="bi bi-arrow-repeat",
+                                    outline=True,
+                                    id="sync_btn"
+                                )
+                            ],style={"width":"100%"}
+                        ),
+                        width=12
+                    )
+                    ])
         ], className='custom-content'
         )
 
     def callbacks(self):
-        self.config.callbacks()
-        #self.track.callbacks()
 
         @app.callback(
             Output("total_year_graph", 'figure'),
@@ -287,7 +335,7 @@ class Content:
                         html.P(
                             "Utilize os filtros para realizar a pesquisa"
                         ),
-                    ], style={"margin-top": "1rem" , "background-color":"#fff", "color":"#0d6efd", "border-color": "#0d6efd"}
+                    ], style={"margin-top": "1rem", "background-color": "#fff", "color": "#0d6efd", "border-color": "#0d6efd"}
                 )
                 return welcome, _filter
             else:
@@ -412,3 +460,107 @@ class Content:
                 ], title=name,
                 ) for name, group in df], start_collapsed=True)
             return accord, _filter
+
+        @ app.callback(
+            Output("upload_alert", "children"),
+            Input('upload_xlsx', 'contents'),
+            State("upload_xlsx", "filename"),
+            prevent_initial_call=True,
+        )
+        def on_button_click(data, filename):
+            content_type, content_string = data.split(',')
+            decoded = base64.b64decode(content_string)
+
+            if filename is None:
+                raise ""
+            else:
+                if 'csv' in filename:
+                    df = pd.read_csv(
+                        StringIO(decoded.decode('utf-8')), sep=";")
+                elif 'xls' in filename:
+                    df = pd.read_excel(
+                        BytesIO(decoded), dtype={'BARCODE': str})
+                else:
+                    return dbc.Alert("FORMATO INVALIDO",
+                                     is_open=True,  duration=4000, color="danger")
+
+                COLUMNS = ('RELEASE_YEAR', 'ARTIST', 'TITLE', 'MEDIA', 'PURCHASE', 'ORIGIN',
+                           'EDITION_YEAR', 'IFPI_MASTERING', 'IFPI_MOULD', 'BARCODE', 'MATRIZ', 'LOTE')
+
+                for col in df.select_dtypes(include=['datetime64']).columns.tolist():
+                    df[col] = df[col].astype(str)
+
+                validate_schema = pa.DataFrameSchema({
+                    "RELEASE_YEAR": pa.Column(int),
+                    "ARTIST": pa.Column(str),
+                    "TITLE": pa.Column(str),
+                    "MEDIA": pa.Column(str),
+                    "PURCHASE": pa.Column(object, nullable=True),
+                    "ORIGIN": pa.Column(str, nullable=True),
+                    "EDITION_YEAR": pa.Column(float, nullable=True),
+                    "IFPI_MASTERING": pa.Column(str, nullable=True),
+                    "IFPI_MOULD": pa.Column(str, nullable=True),
+                    "MATRIZ": pa.Column(str, nullable=True),
+                    "LOTE": pa.Column(str, nullable=True)
+                })
+
+                try:
+                    validate_schema(df)
+                    df.replace({pd.NaT: None, np.nan: None, "NaT": None,
+                                "": None, "None": None}, inplace=True)
+                    df = df.to_dict("records")
+
+                    newList = []
+
+                    for d in df:
+                        newDf = {}
+                        for key, value in d.items():
+                            if key in COLUMNS:
+                                newDf[key] = value
+                        newList.append(newDf)
+
+                    self.conn.drop("CD")
+                    self.conn.insert_many("CD", newList)
+
+                    return dbc.Alert("SALVO",
+                                     is_open=True,  duration=4000)
+                except pa.errors.SchemaError as error:
+                    return dbc.Alert([
+                        html.P(
+                            f"ERRO NOS DADOS DA COLUNA: {error.schema.name}"),
+                        html.P(f"TIPO ESPERADO: {error.check}")
+                    ], is_open=True, color="danger"
+                    )
+
+        @app.callback(
+            Output("sync_alert", "children"),
+            Input('sync_btn', 'n_clicks'),
+            prevent_initial_call=True,
+        )
+        def sync(n_clicks):
+            from data.data_center import MongoDBConn
+            local_conn = MongoDBConn(
+                environ['CONNECTION_STRING_LOCAL'],
+                environ['DATABASE']
+            )
+            df = local_conn.qyery("CD")
+            self.conn.drop("CD")
+            self.conn.insert_many("CD", df)
+            return dbc.Alert("SYNC", is_open=True,  duration=4000)
+
+        @app.callback(
+            Output("download_xlsx", "data"),
+            Input("download_xlsx_btn", "n_clicks"),
+            prevent_initial_call=True,
+        )
+        def on_button_click(n):
+            if n is None:
+                raise ""
+            else:
+                df = self.conn.qyery("CD")
+                df.drop('_id', axis=1, inplace=True)
+                df.drop('DISCOGS', axis=1, inplace=True)
+                df['PURCHASE'] = pd.to_datetime(df['PURCHASE']).dt.date
+                df.replace({pd.NaT: None, np.nan: None, "NaT": None,
+                           "": None, "None": None}, inplace=True)
+                return dcc.send_data_frame(df.to_excel, "collection.xlsx")

d9fbc45eca9d0d3ca9e55def52c35b2ed6aea403
Gabriel Schmidt <gabrieloliveira95@live.com>
2023-02-24 17:55:10 -0300

    Change names

--- src/sidebar.py
+++ src/sidebar.py
@@ -24,7 +24,7 @@ class Sidebar:
                     ]
                 )
             ],
-            className="custom-sidebar-dark"
+            className="custom-sidebar"
         )
 
     def callbacks(self):
